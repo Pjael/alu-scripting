b460ac2407aa99dbfebbfc24c906bd36a89aaee9
root <p.savadogo@alustudent.com>
2024-09-07 10:44:01 -0700

    crrections again

--- regular_expressions/4-repetition_token_3.rb
+++ regular_expressions/4-repetition_token_3.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #regular expression that matches one or more occurence of letter t
-#puts ARGV[0].scan(/hbt*n/).join
+#puts ARGV[0].scan(/hbt+?n/).join
--- regular_expressions/5-beginning_and_end.rb
+++ regular_expressions/5-beginning_and_end.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #regular expression that matches beginnig h and ending n
-#puts ARGV[0].scan(/^h.*n$/).join
+#puts ARGV[0].scan(/h.n/).join
